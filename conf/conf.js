c3c424e69833aafa775d90942eee1b3ba0433f96
JasvinderSingh-Jassi <51856951+JasvinderSingh-Jassi@users.noreply.github.com>
2021-02-24 11:02:52 +0530

    Update conf.js

--- conf/conf.js
+++ conf/conf.js
@@ -13,7 +13,7 @@ exports.config = {
   // Spec patterns are relative to the current working directory when
   // protractor is called.
   //specs: ['..//testcases//XYZ Bank_HomePage_spec.js','..//testcases//XYZ Bank_Customer_Login_spec.js','..//testcases//XYZ Bank_Manager_Login_spec.js'],
-  specs: ['..//testcases2//**.*'],
+  specs: ['..//testcases//**.*'],
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 1000000
